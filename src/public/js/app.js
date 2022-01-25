927a2d0ff408f8a72df131e6de6be7ec6e1a001e
Park Yeonha <49088833+YeonhaPark@users.noreply.github.com>
2022-01-25 11:51:13 -0600

    feat: connect websocket using websocket api and ws library

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,34 @@
-alert('HI')
+const messageList = document.querySelector("ul");
+const nicknameForm = document.querySelector("#nick")
+const messageForm = document.querySelector("#message")
+
+const socket = new WebSocket(`ws://${window.location.host}`)
+
+socket.addEventListener("open", () => {
+  console.log("connected to Server")
+})
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  messageList.append(li);
+})
+ 
+socket.addEventListener("close", () => {
+  console.log("Disconnected from server")
+})
+
+const makeMessage = (type, payload) => {
+  return JSON.stringify({type, payload})
+}
+
+const handleSubmit = (type) => (e) => {
+  e.preventDefault();
+  const form = type === 'nickname' ? nicknameForm : messageForm;
+  const input = form.querySelector("input")
+  socket.send(makeMessage(type, input.value));
+  input.value = ''
+}
+
+nicknameForm.addEventListener("submit", handleSubmit('nickname'));
+messageForm.addEventListener("submit", handleSubmit('message'))
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
 const app = express();
 
@@ -12,4 +14,34 @@ app.get('/*', (_, res) => res.redirect('/'));
 
 const port = 3000;
 
-app.listen(port, () => console.log(`listening at http://localhost:${port}`));
+// creating a server from express application
+const server = http.createServer(app);
+
+// create websocket server ( create both http and websocket server )
+// http and websocket server share same port
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on('connection', (socket) => {
+  // 브라우저별로 소켓이 생성됨
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous"
+  console.log('Connected to Client');
+  socket.on('close', () => console.log('Disconnected from Client'));
+  socket.on('message', (message) => {
+    const { type, payload } = JSON.parse(message);
+
+    switch (type) {
+      case 'message':
+       sockets.forEach(s => s.send(`${socket.nickname}: ${payload}`))
+        break;
+      case 'nickname':
+        socket["nickname"] = payload;
+        break; 
+      default:
+        break;
+    }
+  });
+});
+server.listen(port, () => console.log(`listening at http://localhost:${port}`));
